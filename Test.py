e68da956600e4c6e90ab294fd12608981e706900
gauravramse <gauravramse1@outlook.com>
2021-06-25 09:36:52 +0200

    Working on ation server

---
+++ Test.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get("https://api.covid19india.org/data.json").json()
+
+for i in response['statewise']:
+    if i['state'] == 'Total':
+        print(i['active'])
