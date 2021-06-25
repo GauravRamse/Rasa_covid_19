4f9e636abd1cd8ca0a8e08374b036340cd4e9a62
gauravramse <gauravramse1@outlook.com>
2021-06-25 10:31:24 +0200

    Testing commit

--- Test.py
+++ Test.py
@@ -5,3 +5,8 @@ response = requests.get("https://api.covid19india.org/data.json").json()
 for i in response['statewise']:
     if i['state'] == 'Total':
         print(i['active'])
+
+
+
+
+        000000000000000000000
